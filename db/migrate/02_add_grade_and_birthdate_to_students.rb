d8920776ac86de3bc30923aa55e956f21929d91f
Ali Alsayed <a_alsayed87@hotmail.com>
2018-12-29 07:55:05 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,4 +1,4 @@
-def AddGradeAndBirthdateToStudents < ActiveRecord::Migration[4.2]
+def AddGradeAndBirthdateToStudents < ActiveRecord::Migration[5.1]
   def change
     add_column :artists, :grade, :integer
     add_column :artists, :birthdate, :string
